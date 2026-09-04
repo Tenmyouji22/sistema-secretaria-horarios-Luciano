b6cf277ceb92a4000a77bed40d900c7fcc41bae9
Yogagoma <Yogagoma2006@gmail.com>
2026-09-04 17:39:37 -0400

    Add files via upload
    Corregir errores de sintaxis

--- Representantes-Alumnos-Profesores.sql
+++ Representantes-Alumnos-Profesores.sql
@@ -1,54 +1,54 @@
-CREATE DATABASE IF NOT EXTIST db;
+-- CREATE DATABASE EXISTS db;
 
-// Tabla representantes
-CREATE TABLE IF NOT EXIST representantes(
+-- Tabla representantes
+CREATE TABLE IF NOT EXISTS representantes(
 	id INT PRIMARY KEY,
-	cedula VARCHAR(10),
+	cedula VARCHAR(10) UNIQUE,
 	nombre_completo VARCHAR(70) NOT NULL,
 	telefono VARCHAR(20) NOT NULL,
 	direccion VARCHAR(70) NOT NULL
 );
 
 
-// Tabla alumnos
-CREATE TABLE IF NOT EXIST alumnos(
+-- Tabla alumnos
+CREATE TABLE IF NOT EXISTS alumnos(
 	ID_alumno INT PRIMARY KEY,
-	CI_representante VARCHAR(10)
+	CI_representante VARCHAR(10),
 	cedula_escolar VARCHAR(20) UNIQUE NOT NULL,
 	nombre_completo VARCHAR(70) NOT NULL,
 	fecha_nacimiento DATE NOT NULL,
-	grado INT CHECK,
+	grado INT,
 	seccion VARCHAR(1),
 
-	// Restriccion de grado
+	-- Restriccion de grado
 	CONSTRAINT anio CHECK(grado >= 1 AND grado <= 6),
 	
-	// Rango de secciones(Desde la A hasta la D)
+	-- Rango de secciones(Desde la A hasta la D)
 	CONSTRAINT letra_seccion CHECK(seccion IN ('A', 'B', 'C', 'D')),
 
-	// Clave foranea: Cedula del representante establece relacion 1:N entre representante y alumno
+	-- Clave foranea: Cedula del representante establece relacion 1:N entre representante y alumno
 	CONSTRAINT cedula_representante FOREIGN KEY(CI_representante) REFERENCES representantes(cedula)
 );
 
-// Tabla profesores
-CREATE TABLE IF NOT EXIST profesores(
+-- Tabla profesores
+CREATE TABLE IF NOT EXISTS profesores(
 	ID_profesor INT PRIMARY KEY,
-	cedula VARCHAR(10) PRIMARY KEY,
+	cedula VARCHAR(10) UNIQUE,
 	nombre_completo VARCHAR(70) NOT NULL,
 	especialidad VARCHAR(20) NOT NULL,
 	telefono VARCHAR(20) NOT NULL,
 	salario_base DECIMAL(6,2),
 
-	// Validacion de salario
+	-- Validacion de salario
 	CONSTRAINT chk_salario CHECK(salario_base >= 0)
 );
 
-// Tabla puente para la relacion N:M entre alumnos y profesores
-CREATE TABLE IF NOT EXIST alumno_profesor(
+-- Tabla puente para la relacion N:M entre alumnos y profesores
+CREATE TABLE IF NOT EXISTS alumno_profesor(
 	cedula_alumno VARCHAR(20),
 	CI_profesor VARCHAR(10),
 
-	// Referencias
+	-- Referencias
 	CONSTRAINT fk_alumno FOREIGN KEY(cedula_alumno) REFERENCES alumnos(cedula_escolar),
 	CONSTRAINT fk_profesor FOREIGN KEY(CI_profesor) REFERENCES profesores(cedula)
 );
